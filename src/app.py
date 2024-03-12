590ce32a6645c83ac854b32a065f90d437e8aa22
DavidKN10 <113551618+DavidKN10@users.noreply.github.com>
2024-03-12 13:00:35 -0500

    updated app.py
    added labels, entry box for url, button for choosing location, drop down for the option to save the thumbnail, and a submit button

--- src/app.py
+++ src/app.py
@@ -46,6 +46,39 @@ window.minsize(530, 300)
 window.maxsize(530,300)
 window.title("YouTube to mp4 by Noaxadd")
 
+message = Label(window, text="Welcome!")
+message.place(x=5, y=0)
 
+message = Label(window, text="Enter a YouTube video URL and the video will be saved to your local storage")
+message.place(x=5, y=30)
+
+frame = Frame(window)
+frame.place(x=55, y=70)
+message_frame = LabelFrame(frame, text="Video Info")
+message_frame.grid(row=0, column=0)
+
+test = Label(message_frame, text="Choose location to save video")
+test.grid(row=1, column=0)
+
+test2=Button(message_frame, text="choose location", command=choose_location)
+test2.grid(row=1, column=1)
+
+url_label = Label(message_frame, text="Enter YouTube video URL")
+url_label.grid(row=0,column=0)
+
+url_entry = Entry(message_frame)
+url_entry.grid(row=0, column=1)
+
+x = IntVar()
+check_button = Label(message_frame, text="Save Thumbnail?")
+check_button_combobox = Combobox(message_frame, values=["Yes", "No"])
+check_button.grid(row=2, column=0)
+check_button_combobox.grid(row=2, column=1)
+
+submit_button = Button(message_frame, text='submit', command=submit)
+submit_button.grid(row=3, column=2)
+
+for widget in message_frame.winfo_children():
+    widget.grid_configure(padx=10, pady=5)
 
 window.mainloop()
