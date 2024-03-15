ed01bc67ff4c93acc212c36471743a8c30b782d1
DavidKN10 <davidrs23178@gmail.com>
2024-03-15 14:03:38 -0500

    added feature to save as mp3 or mp4 to gui app

--- src/app.py
+++ src/app.py
@@ -4,9 +4,11 @@ from tkinter.ttk import Combobox
 from yt_to_mp4 import *
 import os
 
+
 file_location = None    #variable for save path
 url = None              #variable for url
 save = None             #variable for option to save thumbnail
+format = None           #variable for choosing a format
 
 #function that will work alongside the 'choose location' button
 #will open a window, so you can choose where to save the video and thumbnail
@@ -23,6 +25,7 @@ def submit():
     #url to save the option that was chosen in the dropdown box
     #will save the option into the variable save    
     save = check_button_combobox.get()
+    format = format_button_combobox.get()
 
     #while loop to continue downloading if told to do so
     #follows similar logic as the while loop in yt2mp4_console.py
@@ -30,12 +33,18 @@ def submit():
         video_url = url
         save_path = file_location
         save_thumbnail = save
+        video_format = format
 
         if not os.path.exists(save_path):
             os.makedirs(save_path)
 
-        #call the function from yt_to_mpy.py to download video
-        download_youtube_mp4(video_url, save_path)
+        # check if format is mp3 or mp4
+        if video_format == "MP4":
+            # call function to download video
+            download_youtube_mp4(video_url, save_path)
+        elif video_format == "MP3":
+            # call function to download video
+            download_youtube_mp3(video_url, save_path)
 
         #if the option to save thumbnail was yes, save thumbnail
         if save_thumbnail == "Yes":
@@ -104,6 +113,14 @@ check_button = Label(message_frame, text="Save Thumbnail?")
 check_button_combobox = Combobox(message_frame, values=["Yes", "No"])
 check_button.grid(row=2, column=0)
 check_button_combobox.grid(row=2, column=1)
+
+#lable for choosing format
+format_button = Label(message_frame, text="Choose a Format")
+
+#combobox that lets you choose between mp3 or mp4
+format_button_combobox = Combobox(message_frame, values=["MP3", "MP4"])
+format_button.grid(row=3, column=0)
+format_button_combobox.grid(row=3, column=1)
 
 #button that when clicked, it will execute the 'submit' function and download the video
 submit_button = Button(message_frame, text='submit', command=submit)
