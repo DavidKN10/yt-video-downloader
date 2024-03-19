9198e51ad10f271691f63edf66cce48604a89f7a
DavidKN10 <davidrs23178@gmail.com>
2024-03-19 00:22:48 -0500

    combined app.py and  yt_to_mp4.py and  added a progress bar to app.py

--- src/app.py
+++ src/app.py
@@ -1,7 +1,8 @@
 from tkinter import *
-from tkinter import filedialog, messagebox
+from tkinter import filedialog, messagebox, ttk
 from tkinter.ttk import Combobox
-from yt_to_mp4 import *
+from pytube import YouTube
+from pythumb import Thumbnail
 import os
 
 
@@ -9,6 +10,90 @@ file_location = None    #variable for save path
 url = None              #variable for url
 save = None             #variable for option to save thumbnail
 format = None           #variable for choosing a format
+
+
+#function to remove invalid characters from a video title so that it can be properly downloaded
+def clean_filename(filename):
+    #remove invalid characters
+    invalid_chars = r'\/:*?<>|"'
+    invalid_characters = set(invalid_chars)
+
+    # make a new string with invalid characters removed
+    # if the character is in the set of invalid_characters, then it will be replaced with '_'
+    cleaned_text = ''.join(['_' if char in invalid_characters else char for char in filename])
+
+    #return the file name with invalid characters removed
+    return cleaned_text
+
+
+#function to download the YouTube video as mp4 given the url and download path
+def download_youtube_mp4(url, save_path):
+    try:
+        #create a YouTube object
+        yt = YouTube(url, on_progress_callback=on_progress)
+
+        #get the video title
+        video_title = clean_filename(yt.title)
+
+        #choose the highest resolution stream
+        stream = yt.streams.get_highest_resolution()
+
+        #download stream to the specified path
+        stream.download(output_path=save_path, filename=f"{video_title}.mp4")
+
+    except Exception as e:
+        print("An error occurred:", str(e))
+
+
+# function to download the YouTube video as mp3 given the url and download path
+def download_youtube_mp3(url, save_path):
+    try:
+        # create a YouTube object
+        yt = YouTube(url, on_progress_callback=on_progress)
+
+        # get the video title
+        video_title = clean_filename(yt.title)
+
+        # choose the highest resolution stream
+        stream = yt.streams.get_highest_resolution()
+
+        # download stream to the specified path
+        stream.download(output_path=save_path, filename=f"{video_title}.mp3")
+    except Exception as e:
+        print("An error occurred:", str(e))
+
+
+def on_progress(stream, chunk, bytes_remaining):
+    total_size = stream.filesize
+    bytes_downloaded = total_size - bytes_remaining
+    percent_complete = (bytes_downloaded / total_size) * 100
+    percent = str(int(percent_complete))
+    percent_label.configure(text=percent+'%')
+    percent_label.update()
+
+    #update progress bar
+    progress_bar['value'] = float(percent_complete)
+
+
+# function to download the YouTube video thumbnail given the url and download path
+def download_youtube_thumbnail(url, save_path):
+    try:
+        # create YouTube object
+        yt = YouTube(url)
+
+        # get the video title
+        thumbnail_filename = clean_filename(yt.title)
+
+        # get the highest resolution of thumbnail, maxresdefault
+        thumbnail = Thumbnail(url)
+        thumbnail.fetch(size="maxresdefault")
+
+        # save thumbnail to the specified save path
+        thumbnail.save(dir=save_path, filename=thumbnail_filename)
+
+    except Exception as e:
+        print("An error occurred:", str(e))
+
 
 #function that will work alongside the 'choose location' button
 #will open a window, so you can choose where to save the video and thumbnail
@@ -20,11 +105,15 @@ def choose_location():
 #function that will work alongside the 'submit' button
 def submit():
     #get url from the entry box and save it to url    
+    global url
     url = url_entry.get()
 
     #url to save the option that was chosen in the dropdown box
     #will save the option into the variable save    
+    global save
     save = check_button_combobox.get()
+
+    global format
     format = format_button_combobox.get()
 
     #while loop to continue downloading if told to do so
@@ -38,6 +127,9 @@ def submit():
         if not os.path.exists(save_path):
             os.makedirs(save_path)
 
+        #if the option to save thumbnail was yes, save thumbnail
+        if save_thumbnail == "Yes":
+            download_youtube_thumbnail(video_url, save_path)
         # check if format is mp3 or mp4
         if video_format == "MP4":
             # call function to download video
@@ -45,10 +137,6 @@ def submit():
         elif video_format == "MP3":
             # call function to download video
             download_youtube_mp3(video_url, save_path)
-
-        #if the option to save thumbnail was yes, save thumbnail
-        if save_thumbnail == "Yes":
-            download_youtube_thumbnail(video_url, save_path)
 
         #When the video and thumbnail finish downloading, a message box will appear.
         #if you click yes, message box closes, and you can download another video
@@ -64,6 +152,8 @@ def submit():
 
         #if you click yes, end while loop only
         if answer:
+            progress_bar['value'] = 0
+            percent_label.configure(text="0%")
             break
 
 
@@ -71,8 +161,8 @@ window = Tk()
 #making the icon, window, and title
 window.iconbitmap("V33983897_on_X.ico")
 window.geometry('530x500')
-window.minsize(530, 300)
-window.maxsize(530,300)
+window.minsize(530, 400)
+window.maxsize(530,400)
 window.title("YouTube to mp4 by Noaxadd")
 
 #labels at the top of window that give instructions
@@ -114,7 +204,7 @@ check_button_combobox = Combobox(message_frame, values=["Yes", "No"])
 check_button.grid(row=2, column=0)
 check_button_combobox.grid(row=2, column=1)
 
-#lable for choosing format
+#label for choosing format
 format_button = Label(message_frame, text="Choose a Format")
 
 #combobox that lets you choose between mp3 or mp4
@@ -128,5 +218,17 @@ submit_button.grid(row=3, column=2)
 
 for widget in message_frame.winfo_children():
     widget.grid_configure(padx=10, pady=5)
+
+#new frame to add the progress bar
+frame2 = Frame(window)
+frame2.place(x=55, y=275)
+message_frame2 = LabelFrame(frame2, text="Progress")
+message_frame2.grid(row=0, column=0)
+
+percent_label = Label(frame2, text="0%")
+percent_label.grid(row=1, column=0)
+#progress bar
+progress_bar = ttk.Progressbar(frame2, orient=HORIZONTAL, length=400, mode='determinate')
+progress_bar.grid(row=2, column=0, padx=10, pady=10)
 
 window.mainloop()
